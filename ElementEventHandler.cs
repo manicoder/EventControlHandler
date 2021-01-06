f3f75669015fc2d367ade1a489f7096269e00a97
Manoj Kumar <manojkg@live.com>
2021-01-06 18:59:09 +0700

    Add project files.

---
+++ ElementEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventControlHandler
+{
+    public class ElementEventHandler
+    {
+        public string ElementName { get; set; }
+        public string ElementValue { get; set; }
+        public double ScrollX { get; set; }
+        public double ScrollY { get; set; }
+        public ElementType ElementType { get; set; }
+        public string ToUserId { get; set; }
+        public bool IsExpert { get; set; }
+    }
+    public enum ElementType
+    {
+        TextBox,
+        Button,
+        ScroolView
+    }
+}
---
+++ Hubs/SendMessageHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventControlHandler.Hubs
+{
+    public class SendMessageHub : Hub
+    {
+        public async Task ElementEventSendMessageFromServer(ElementEventHandler values)
+        {
+            await Clients.All.SendAsync("ElementEventReceiveMessage", values);
+        }
+    }
+}
